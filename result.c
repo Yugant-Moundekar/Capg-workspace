ecf148fc6eee2c60def62810c7a3b3987510e654
Yugant-Moundekar <yugantmoundekar6320@gmail.com>
2023-06-20 16:11:24 +0530

    Nakul code first commit

---
+++ result.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main()
+{
+    int a,b;
+    char c;
+    printf("\t\t\tEnter the name = ");
+    scanf("%6c",&c);
+    printf("\t\t\tEnter the rolls number = ");
+    scanf("%d",&a);
+    printf("\t\tSubject\t\tMark");
+    
+    // printf("the addition is %d",a+b);
+    return 0;
+}
